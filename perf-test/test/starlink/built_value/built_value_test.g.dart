66ff413a9a88cca7a3273d4e6aad2a4fe17d9673
Alexander Mazuruk <alexander.mazuruk@gmail.com>
2021-07-11 23:56:45 +0200

    update test

--- perf-test/test/starlink/built_value/built_value_test.g.dart
+++ perf-test/test/starlink/built_value/built_value_test.g.dart
@@ -298,7 +298,7 @@ class _$SpaceTrackSerializer implements StructuredSerializer<SpaceTrack> {
           break;
         case 'DECAY_DATE':
           result.dECAYDATE = serializers.deserialize(value,
-              specifiedType: const FullType(String)) as String;
+              specifiedType: const FullType(String)) as String?;
           break;
         case 'DECAYED':
           result.dECAYED = serializers.deserialize(value,
@@ -1161,4 +1161,4 @@ class RecordBuilder implements Builder<Record, RecordBuilder> {
   }
 }
 
-// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,lines_longer_than_80_chars,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new
+// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,deprecated_member_use_from_same_package,lines_longer_than_80_chars,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new
